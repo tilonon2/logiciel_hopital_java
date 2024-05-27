4d25c14e95ec57c8e0f5524913dd8ed12ecb9dbb
Mouhammed Sad Radji <saadradji11@gmail.com>
2024-05-27 05:57:31 +0000

    Chart
    Graphique

--- src/main/java/com/example/hopital/dashController.java
+++ src/main/java/com/example/hopital/dashController.java
@@ -1,8 +1,13 @@
 package com.example.hopital;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import model.Book;
@@ -21,9 +26,15 @@ public class dashController implements Initializable {
     private VBox bookContainer;
     private List<Book> recentlyAdded;
     private List<Book> booksVertical;
-
+    @FXML
+    private LineChart<?, ?> lineChart;
+    @FXML
+    private PieChart pieChart;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        iniLineChart();
+        iniPieChart();
+
         recentlyAdded = new ArrayList<>(recentlyAdded());
         booksVertical = new ArrayList<>(booksVertical());
 
@@ -49,6 +60,30 @@ public class dashController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void iniPieChart() {
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+        new PieChart.Data("Android", 15),
+        new PieChart.Data("JavaFx", 30),
+        new PieChart.Data("Java Swing", 10),
+        new PieChart.Data("C", 3),
+        new PieChart.Data("React Native", 2));
+        pieChart.setData(pieChartData);
+    }
+
+    private void iniLineChart() {
+        XYChart.Series series = new XYChart.Series();
+        series.getData().add(new XYChart.Data( "Monday", 8));
+        series.getData().add(new XYChart.Data( "Tuesday", 12));
+        series.getData().add(new XYChart.Data( "Wednesday", 10));
+        series.getData().add(new XYChart.Data( "Thursday", 15));
+        series.getData().add(new XYChart.Data( "Friday", 12));
+        series.getData().add(new XYChart.Data( "Saturday", 8));
+        series.getData().add(new XYChart.Data( "Sunday", 5));
+        lineChart.getData().addAll(series);
+        lineChart.lookup(".chart-plot-background").setStyle("fx-background-color: transparent;");
+        series.getNode().setStyle("fx-stroke: #2F3192");
     }
 
     private List<Book> recentlyAdded() {
